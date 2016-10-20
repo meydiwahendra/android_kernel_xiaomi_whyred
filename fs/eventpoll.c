eb989ebf773847b74ed5c30ea0d19ea36ed7ce99
John Dias <joaodias@google.com>
2016-10-20 10:28:32 -0700

    fs: add useful debug info to eventpoll string
    Add pid and file to facilitate debugging when eventpoll wakeup
    prevents sleeping.
    
    Bug: 32180327
    Change-Id: I34bada5ddab04cf3830762c745f46bfcd1549cb8
    Signed-off-by: John Dias <joaodias@google.com>

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1236,15 +1236,16 @@ static int ep_create_wakeup_source(struct epitem *epi)
 	struct name_snapshot n;
 	struct wakeup_source *ws;
 
+	name = epi->ffd.file->f_path.dentry->d_name.name;
 	if (!epi->ep->ws) {
-		epi->ep->ws = wakeup_source_register("eventpoll");
+		char buf[64];
+		snprintf(buf, sizeof(buf), "eventpoll pid:%d file:%s", current->pid, name);
+		epi->ep->ws = wakeup_source_register(buf);
 		if (!epi->ep->ws)
 			return -ENOMEM;
 	}
 
-	take_dentry_name_snapshot(&n, epi->ffd.file->f_path.dentry);
-	ws = wakeup_source_register(n.name);
-	release_dentry_name_snapshot(&n);
+	ws = wakeup_source_register(name);
 
 	if (!ws)
 		return -ENOMEM;
