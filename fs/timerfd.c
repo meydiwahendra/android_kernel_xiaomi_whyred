56d27f63754f896902973b0732aa5062a956712c
Kelly Rossmoyer <krossmo@google.com>
2017-10-19 13:24:10 -0700

    fs: Improve eventpoll logging to stop indicting timerfd
    timerfd doesn't create any wakelocks; eventpoll can, and is creating the
    wakelocks we see called "[timerfd]".  eventpoll creates two kinds of
    wakelocks: a single top-level lock associated with the eventpoll fd
    itself, and one additional lock for each fd it is polling that needs such
    a lock (e.g. those using EPOLLWAKEUP).  Current code names the per-fd
    locks using the undecorated names of the fds' associated files (hence
    "[timerfd]"), and is naming the top-level lock after the PID of the caller
    and the name of the file behind the first fd for which a per-fd lock is
    created.  To make things clearer, the top-level lock is now named using
    the caller PID and an "epollfd" designation, while the per-fd locks are
    also named with the caller's PID (to associate them with the top-level
    lock) and their respective fds' file names.
    
    Bug: 63622255
    Bug: 38042165
    Test: Ran on device and observed new wakelock naming in bugreport, dumpsys
    batterystats, /d/tracing/trace, and d/wakeup_reasons.
    Change-Id: I32bfe5fe6766bb569329a450cc7163d52d59ed34
    Signed-off-by: Kelly Rossmoyer <krossmo@google.com>

--- fs/timerfd.c
+++ fs/timerfd.c
@@ -25,6 +25,7 @@
 #include <linux/syscalls.h>
 #include <linux/compat.h>
 #include <linux/rcupdate.h>
+#include <linux/hrtimer.h>
 
 struct timerfd_ctx {
 	union {
@@ -43,6 +44,8 @@ struct timerfd_ctx {
 	spinlock_t cancel_lock;
 	bool might_cancel;
 };
+
+static atomic_t instance_count = ATOMIC_INIT(0);
 
 static LIST_HEAD(cancel_list);
 static DEFINE_SPINLOCK(cancel_lock);
@@ -388,6 +391,11 @@ SYSCALL_DEFINE2(timerfd_create, int, clockid, int, flags)
 	int ufd;
 	struct timerfd_ctx *ctx;
 
+	enum alarmtimer_type type;
+	char task_comm_buf[TASK_COMM_LEN];
+	char file_name_buf[32];
+	int instance;
+
 	/* Check the TFD_* constants for consistency.  */
 	BUILD_BUG_ON(TFD_CLOEXEC != O_CLOEXEC);
 	BUILD_BUG_ON(TFD_NONBLOCK != O_NONBLOCK);
@@ -423,7 +431,12 @@ SYSCALL_DEFINE2(timerfd_create, int, clockid, int, flags)
 
 	ctx->moffs = ktime_mono_to_real((ktime_t){ .tv64 = 0 });
 
-	ufd = anon_inode_getfd("[timerfd]", &timerfd_fops, ctx,
+	instance = atomic_inc_return(&instance_count);
+	get_task_comm(task_comm_buf, current);
+	snprintf(file_name_buf, sizeof(file_name_buf), "[timerfd%d_%.*s]",
+		 instance, (int)sizeof(task_comm_buf), task_comm_buf);
+
+	ufd = anon_inode_getfd(file_name_buf, &timerfd_fops, ctx,
 			       O_RDWR | (flags & TFD_SHARED_FCNTL_FLAGS));
 	if (ufd < 0)
 		kfree(ctx);
