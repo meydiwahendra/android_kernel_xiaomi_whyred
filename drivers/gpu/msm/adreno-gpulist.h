2054d477cbe0b8ee18e98236542015107a350e9d
Edwin Moquete <edwinmmoquete@gmail.com>
2020-12-09 22:40:55 -0400

    msm: kgsl: Use a540 VBIF registers for a509/512
    Commit 03e92d7 moved VBIF settings to the GPU list, but
    didn't account for a512. a509 needs them as well.
    
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- drivers/gpu/msm/adreno-gpulist.h
+++ drivers/gpu/msm/adreno-gpulist.h
@@ -608,23 +608,6 @@ static const struct adreno_a5xx_core adreno_gpu_core_a512 = {
 	.vbif_count = ARRAY_SIZE(a540_vbif_regs),
 };
 
-static const struct adreno_a5xx_core adreno_gpu_core_a509 = {
-	.base = {
-		DEFINE_ADRENO_REV(ADRENO_REV_A509, 5, 0, 9, ANY_ID),
-		.features = ADRENO_PREEMPTION | ADRENO_64BIT |
-			ADRENO_CONTENT_PROTECTION | ADRENO_CPZ_RETENTION,
-		.gpudev = &adreno_a5xx_gpudev,
-		.gmem_size = (SZ_256K + SZ_16K),
-		.busy_mask = 0xfffffffe,
-		.bus_width = 32,
-	},
-	.pm4fw_name = "a530_pm4.fw",
-	.pfpfw_name = "a530_pfp.fw",
-	.zap_name = "a512_zap",
-	.hwcg = a512_hwcg_regs,
-	.hwcg_count = ARRAY_SIZE(a512_hwcg_regs),
-};
-
 static const struct adreno_a5xx_core adreno_gpu_core_a508 = {
 	.base = {
 		DEFINE_ADRENO_REV(ADRENO_REV_A508, 5, 0, 8, ANY_ID),
@@ -659,6 +642,8 @@ static const struct adreno_a5xx_core adreno_gpu_core_a509 = {
 	.zap_name = "a512_zap",
 	.hwcg = a512_hwcg_regs,
 	.hwcg_count = ARRAY_SIZE(a512_hwcg_regs),
+	.vbif = a540_vbif_regs,
+	.vbif_count = ARRAY_SIZE(a540_vbif_regs),
 };
 
 DEFINE_DEPRECATED_CORE(a630v1, ADRENO_REV_A630, 6, 3, 0, 0);
